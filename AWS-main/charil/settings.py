618ab5dca4910867dd0a334f735841cdca6fff09
Karthik_2307 <125734894+karthik-2307@users.noreply.github.com>
2023-08-25 19:07:28 +0530

    Update settings.py

--- AWS-main/charil/settings.py
+++ AWS-main/charil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)j0l1%3++uum(fhls(x0q^)24n^-+ff9l_6c+vb!ylup)zcnm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.110.94.112','localhost','0.0.0.0']
 
 
 # Application definition
